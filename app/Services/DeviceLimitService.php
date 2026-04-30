8a327d762fabb67df407084192186efdd6c3c5ff
ganapurba007 <ganapurbakusuma86@gmail.com>
2026-04-30 09:53:59 +0700

    feat: add service DeviceLimit with jenssegers agent

---
+++ app/Services/DeviceLimitService.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use App\Models\UserDevice;
+use Illuminate\Support\Str;
+use Jenssegers\Agent\Facades\Agent;
+
+
+
+
+class DeviceLimitService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function registerDevice(User $user)
+    {
+        $deviceInfo = $this->getDeviceInfo();
+        $exsitingDevice = $this->findExsitingDevice($user, $deviceInfo);
+
+        if ($exsitingDevice) {
+            $exsitingDevice->update(['last_active' => now()]);
+            session(['device_id' => $exsitingDevice->id]);
+            return $exsitingDevice;
+        }
+
+        if ($this->hasReachedDeviceLimit($user)) {
+            return false;
+        }
+
+        $device = $this->createNewDevice($user, $deviceInfo);
+        session(['device_id' => $device->id]);
+        return $device;
+    }
+
+    private function getDeviceInfo()
+    {
+        return [
+            'device_name' => $this->generateDeviceName(),
+            'device_type' => Agent::isDesktop() ? 'desktop' : (Agent::isPhone() ? 'phone' : 'tablet'),
+            'platform' => Agent::platform(),
+            'platform_version' => Agent::version(Agent::platform()),
+            'browser' => Agent::browser(),
+            'browser_version' => Agent::version(Agent::browser())
+        ];
+    }
+
+    private function generateDeviceName()
+    {
+        return ucfirst(Agent::platform()) . ' ' . ucfirst(Agent::browser());
+    }
+
+    private function findExsitingDevice(User $user, array $deviceInfo)
+    {
+        return UserDevice::where('user_id', $user->id)
+            ->where('device_name', $deviceInfo['device_name'])
+            ->wheere('platform', $deviceInfo['platform'])
+            ->where('browser', $deviceInfo['browser'])
+            ->first();
+    }
+
+    private function hasReachedDeviceLimit(User $user)
+    {
+        $maxDevices = $user->getCurrenPlan()->max_devices ?? 1;
+        return UserDevice::where('user_id', $user->id)->count() >= $maxDevices;
+    }
+
+    private function createNewDevice(User $user, array $deviceInfo)
+    {
+        return UserDevice::create([
+            'user_id' => $user->id,
+            'device_id' => $this->generateDeviceId(),
+            'device_name' => $deviceInfo['device_name'],
+            'device_type' => $deviceInfo['device_type'],
+            'platform' => $deviceInfo['platform'],
+            'platform_version' => $deviceInfo['platform_version'],
+            'browser' => $deviceInfo['browser'],
+            'browser_version' => $deviceInfo['browser_version'],
+            'last_active' => now()
+        ]);
+    }
+
+    private function generateDeviceId()
+    {
+        return Str::random(32);
+    }
+}
